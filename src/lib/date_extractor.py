6d68c7ba7b4384283582e566d40dd1b7ce8ec9ce
Noah Hood <hood.noah@icloud.com>
2024-07-06 13:16:45 -0400

    add OCR for scanned documents

--- src/lib/date_extractor.py
+++ src/lib/date_extractor.py
@@ -5,7 +5,7 @@ import re
 import fitz
 
 # types
-from typing import Optional
+from typing import List, Optional
 
 MAX_NUM_SEARCH_PAGES = 3
 
@@ -19,23 +19,40 @@ def _parse_date_string(date_string: str) -> Optional[date]:
     return date
 
 
-def get_report_date(filepath: Path) -> Optional[date]:
-    # open file
+def is_scanned_file(doc: fitz.Document) -> bool:
+    # try and locate any text on the first 3 pages
+    for page_num in range(min(MAX_NUM_SEARCH_PAGES, len(doc))):
+        if doc.get_page_text(page_num) == "":
+            return True
 
-    # read text from first 3 pages
+    return False
+
+
+def get_text(doc: fitz.Document) -> str:
+    num_search_pages = min(MAX_NUM_SEARCH_PAGES, len(doc))
+
     text = ""
-    with fitz.open(filepath) as file:
-        for page_num in range(min(3, len(file))):
-            text += file.get_page_text(page_num)
-
-    dates_text = re.findall(DATE_REGEX, text)
-    print(dates_text)
-    dates_parsed = [_parse_date_string(text) for text in dates_text]
-    dates_parsed_not_none = [
-        parsed_date for parsed_date in dates_parsed if parsed_date is not None
-    ]
-
-    if dates_parsed_not_none and len(dates_parsed_not_none) > 0:
-        return max(dates_parsed_not_none)
+    if not is_scanned_file(doc):
+        for page_num in range(num_search_pages):
+            text += doc.get_page_text(page_num)
     else:
-        return None
+        page = doc.load_page(2).get_textpage_ocr(dpi=200, full=True)
+        text += page.extractText()
+
+    return text
+
+
+def get_report_date(filepath: Path) -> Optional[date]:
+    with fitz.open(filepath) as doc:
+        text = get_text(doc)
+
+        dates_text = re.findall(DATE_REGEX, text)
+        dates_parsed = [_parse_date_string(text) for text in dates_text]
+        dates_parsed_not_none = [
+            parsed_date for parsed_date in dates_parsed if parsed_date is not None
+        ]
+
+        if dates_parsed_not_none and len(dates_parsed_not_none) > 0:
+            return max(dates_parsed_not_none)
+        else:
+            return None
